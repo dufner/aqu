90fd8838066aef0a458fbfb77e7c4c045e50909f
Dufner <dufner@mail.ru>
2023-05-31 11:52:16 +0300

    first commit

--- src/main/java/com/example/demo/web/HomeController.java
+++ src/main/java/com/example/demo/web/HomeController.java
@@ -1,7 +1,14 @@
 package com.example.demo.web;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-public class HomeController  {
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+public class HomeController implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
 }
